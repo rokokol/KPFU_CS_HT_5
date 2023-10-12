154cd4d1df484b6db2612c0fd147a206ea215a38
rokokol <mailofilyusha@gmail.com>
2023-10-12 19:18:24 +0300

    Tasks

--- HT/Program.cs
+++ HT/Program.cs
@@ -1,12 +1,86 @@
 ﻿using System;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace HT
 {
-    class MainClass
+    class MainClass 
     {
+        /// <summary>
+        /// Writes "> " in start of the line.
+        /// </summary>
+        static void Offer()
+        {
+            Console.Write("> ");
+        }
+
+        /// <summary>
+        /// Writes a number of the task.
+        /// </summary>
+        /// <param name="message">Message.</param>
+        /// <param name="number">Number of the task.</param>
+        static void Message(string message, int number)
+        {
+            Console.WriteLine("\nLet's check problem #{0}\nThis program {1}\nPress any to continue...", number, message);
+            Console.ReadKey();
+        }
+
+        /// <summary>
+        /// Reads the input int. If input incorrect it ass user to try again.
+        /// </summary>
+        /// <returns>The input int.</returns>
+        /// <param name="positiveFlag">If set to <c>true</c> input must be positive.</param>
+        /// <param name="nonZero">If set to <c>true</c> input must not be zero.</param>
+        static int ReadInt(bool positiveFlag, bool nonZero)
+        {
+            int result = 1;
+            bool term = true;
+            while (term)
+            {
+                Offer();
+                bool convert = int.TryParse(Console.ReadLine(), out result);
+                bool positive = (result >= 0) || !positiveFlag;
+                bool noZero = (result != 0) || !nonZero;
+                if (convert && positive && noZero)
+                {
+                    term = false;
+                }
+                else if (!positive)
+                {
+                    Console.WriteLine("The input must be positive. Please, try again:");
+                }
+                else
+                {
+                    Console.WriteLine("Incorrect input. Please, try again:");
+                }
+            }
+            return result;
+        }
+
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            void Problem1()
+            {
+                Message("shuffles an array of 32 pairs of images and prints it", 1);
+                Image image = new Image();
+            }
+
+            bool run = true;
+            while (run)
+            {
+                Console.WriteLine();
+                Console.WriteLine("||===========================<\\\\>===========================||");
+                Console.WriteLine("Please, input a number of a task (1-4) or type \"exit\" to stop:");
+                Offer();
+                switch (Console.ReadLine())
+                {
+                    case "1": Problem1(); break;
+                    //case "2": Problem2(); break;
+                    //case "3": Problem3(); break;
+                    //case "4": Problem4(); break;
+                    case "exit": run = false; break;
+                    default: Console.WriteLine("This is not a command or a number of task"); break;
+                }
+            }
         }
     }
 }
